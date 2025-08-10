7b7746c6bef6b0370a2d6ab77dfa27c583e51587
Sapient44 <doineedaname18@gmail.com>
2025-08-10 15:21:39 +0530

    Sets up a basic Hello World type program in pyscript
    Since setting pyscript by itself takes a bit of time, this commit acts as basic Hello World, so everyone can start coding directly.
    The __init__.py is a flask server.
    The files in static would be our frontend assets.
    The files in templates would be our html, since flask demands it in such a way for it's render_template function to work

---
+++ lib/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Create a Flask app instance
+app = Flask(__name__)
+
+
+# Define a route for the homepage
+@app.route("/")
+def home() -> str:
+    """Execute the base function."""
+    return render_template("index.html")
+
+
+# Start the server on localhost:8000
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)  # noqa: S104, S201
---
+++ lib/static/frontend.py
@@ -0,0 +1,4 @@
+from js import document  # noqa: INP001
+
+print("Hello from Python!")  # This will show in the browser console
+document.getElementById("result").innerHTML = "This message is from Python!"
